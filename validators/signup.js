926ecb23d4dd6516f1e50e70861997e97d109696
tylerbare98 <tylerbare98@gmail.com>
2022-07-05 18:55:22 +0000

    Finished login validation

--- validators/signup.js
+++ validators/signup.js
@@ -5,6 +5,11 @@ exports.signup = function(req, res) {
     
     return [
         check('name', 'Name is required').notEmpty(),
+        check('email', 'Invalid email').isEmail(),
         check('password', 'Password is required').notEmpty(),
+        check("password", "Password must include one lowercase character," +
+            " one uppercase character, a number, a special character," +
+            " and be 8 chracters long")
+            .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i")
     ]
 };
